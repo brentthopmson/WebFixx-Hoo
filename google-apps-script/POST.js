c3df307cb719c43745cbf545447193b2ab013608
robertlugana <brentthopmson@gmail.com>
2026-09-09 14:24:22 -0500

    fix: FOLDER_ID.HUB points to actual HUB folder

--- google-apps-script/POST.js
+++ google-apps-script/POST.js
@@ -16,7 +16,7 @@ const CONFIG = {
     USERS: "1EpxeonSw7wrvzSjiqRPKkIFRUHoZR_Pp",
     PROFILE_PICTURE: "1TwKBloBke5GQav9h5knkO0lk7ezo-PR8",
     CAMPAIGNS: "1ndSyFTzxAiWrAknA_H5gutcJf-kejfeY",
-    HUB: "1Xo6HALrBHOtky-d25OTdTgYtBXfV5Wwi",
+    HUB: "1l46cW8SzinsRFC8OH3v9eVplSbda7CBF",
   },
   CACHE_EXPIRED_IN_SECONDS: 21600, // 6 hours
   EXTERNAL_API: "https://670d-41-190-12-74.ngrok-free.app"
